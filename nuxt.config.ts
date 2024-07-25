aefe820a828b06472b5c6463b6aad5c317b81a93
Martin Gonzalez <mtn.gm05@gmail.com>
2024-07-25 09:42:44 -0600

    Upload project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,13 +10,13 @@ export default defineNuxtConfig({
       sessionCookie: false
     },
     config: {
-      apiKey: "AIzaSyCSsgTcJBCk72yrPgdCeIKIGy9aX1Le4GE",
-      authDomain: "nuxt-auth-a55bb.firebaseapp.com",
-      projectId: "nuxt-auth-a55bb",
-      storageBucket: "nuxt-auth-a55bb.appspot.com",
-      messagingSenderId: "289646177408",
-      appId: "1:289646177408:web:ee5aa069ec0585dcdeca65",
-      measurementId: "G-8VG4XXP7MC"
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+      measurementId: ""
     }
   },
 
